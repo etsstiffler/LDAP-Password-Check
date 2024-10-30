09d98b3322db3bf9e8fb17e4df034d0f84ec7028
fdr <dr@werkgymnasium.de>
2024-10-30 11:59:44 +0100

    phpmailer

--- main.php
+++ main.php
@@ -3,12 +3,11 @@
 # Email: dr@Werkgymnasium.de
 # Juli 2023
 
-# Import PHPMailer classes into the global namespace
-# These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-# Load Composer's autoloader
+//Load Composer's autoloader
 require 'vendor/autoload.php';
 
 # Zeitzone festlegen
